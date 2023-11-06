4432dc85a719c902f398d9d56d4c3d2f957bed83
Barnabé <barnabe.mitaut@epfedu.fr>
2023-11-06 11:13:25 +0100

    envoie du projet graphique

---
+++ LightOff_MITAUT_version_graphique/src/lightoff_mitaut_version_console/LightOff_MITAUT_version_console.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightoff_mitaut_version_console;
+
+/**
+ *
+ * @author Barnabé
+ */
+public class LightOff_MITAUT_version_console {
+
+
+    public static void main(String[] args) {
+       Partie partie1= new Partie();
+       partie1.initialiserPartie();
+        partie1.lancerPartie();
+       
+    }
+   
+}
